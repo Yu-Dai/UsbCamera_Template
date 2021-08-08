59415ab8cc8811828b949b74806b2f99009ae577
Yu-Dai <61172609+Yu-Dai@users.noreply.github.com>
2021-08-09 02:35:50 +0800

    FirstCommit
    FirstCommit

---
+++ SpertroApp/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SpertroApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CCDImage = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.DrawCanvas = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ROIImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CCDImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DrawCanvas)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ROIImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CCDImage
+            // 
+            this.CCDImage.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CCDImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CCDImage.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.CCDImage.Location = new System.Drawing.Point(2, 2);
+            this.CCDImage.Margin = new System.Windows.Forms.Padding(0);
+            this.CCDImage.Name = "CCDImage";
+            this.CCDImage.Size = new System.Drawing.Size(480, 288);
+            this.CCDImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CCDImage.TabIndex = 1;
+            this.CCDImage.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(2, 401);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 53);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // DrawCanvas
+            // 
+            this.DrawCanvas.BackColor = System.Drawing.Color.Transparent;
+            this.DrawCanvas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.DrawCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DrawCanvas.Location = new System.Drawing.Point(36, 38);
+            this.DrawCanvas.Margin = new System.Windows.Forms.Padding(0);
+            this.DrawCanvas.MinimumSize = new System.Drawing.Size(8, 9);
+            this.DrawCanvas.Name = "DrawCanvas";
+            this.DrawCanvas.Size = new System.Drawing.Size(63, 38);
+            this.DrawCanvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DrawCanvas.TabIndex = 4;
+            this.DrawCanvas.TabStop = false;
+            this.DrawCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawCanvas_MouseDown);
+            this.DrawCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DrawCanvas_MouseMove);
+            this.DrawCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DrawCanvas_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.ROIImage);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(2, 458);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 316);
+            this.panel1.TabIndex = 6;
+            // 
+            // ROIImage
+            // 
+            this.ROIImage.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ROIImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ROIImage.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ROIImage.Location = new System.Drawing.Point(-44, 14);
+            this.ROIImage.Margin = new System.Windows.Forms.Padding(0);
+            this.ROIImage.Name = "ROIImage";
+            this.ROIImage.Size = new System.Drawing.Size(480, 288);
+            this.ROIImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ROIImage.TabIndex = 6;
+            this.ROIImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1171, 801);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DrawCanvas);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CCDImage);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.CCDImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DrawCanvas)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ROIImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CCDImage;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox DrawCanvas;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ROIImage;
+
+    }
+}
+
---
+++ SpertroApp/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GitHub.secile.Video;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace SpertroApp
+{
+
+    public partial class Form1 : Form
+    {
+        //Camera
+        UsbCamera camera;
+
+        //Live Stop
+        public bool bCameraLive = false;
+
+        //ImageBuff
+        //private byte[] GrayBuffer;
+
+        //ROI
+        Point ROI_Point;
+        const int ROI_Edge = 10;
+
+        private object BufferLock = new object();
+        private Bitmap CopyBitmap(byte[] Buffer,int width, int height)
+        {
+            
+            var result = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            if (Buffer == null) return result;
+
+            var bmp_data = result.LockBits(new Rectangle(Point.Empty, result.Size), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            lock (BufferLock)
+            {
+                // copy from last row.
+                for (int y = 0; y < height; y++)
+                {
+                    int stride = width* 3;
+
+                    var src_idx = Buffer.Length - (stride * (y + 1));
+                    var dst = IntPtr.Add(bmp_data.Scan0, stride * y);
+                    
+                    Marshal.Copy(Buffer, src_idx, dst, stride);
+
+                }
+            }
+            result.UnlockBits(bmp_data);
+
+            return result;
+        }
+
+        //convert image to bytearray
+        public static byte[] ImageToBuffer(Image Image, System.Drawing.Imaging.ImageFormat imageFormat)
+        {
+            if (Image == null) { return null; }
+            byte[] data = null;
+            using (MemoryStream oMemoryStream = new MemoryStream())
+            {
+                //建立副本
+                using (Bitmap oBitmap = new Bitmap(Image))
+                {
+                    //儲存圖片到 MemoryStream 物件，並且指定儲存影像之格式
+                    oBitmap.Save(oMemoryStream, imageFormat);
+                    //設定資料流位置
+                    oMemoryStream.Position = 0;
+                    //設定 buffer 長度
+                    data = new byte[oMemoryStream.Length];
+                    //將資料寫入 buffer
+                    oMemoryStream.Read(data, 0, Convert.ToInt32(oMemoryStream.Length));
+                    //將所有緩衝區的資料寫入資料流
+                    oMemoryStream.Flush();
+                }
+            }
+            return data;
+        }
+        delegate void FormUpdata(int i);
+        void FormUpdataMethod(int i)
+        {
+            Text = i.ToString();
+
+        }
+        //private async Task<bool> SpertroImageProcess(int i)
+        private async Task<bool> SpertroImageProcess(byte[] imgbuffer)
+        {
+            //await Thread.Sleep(2000);
+            //await Task.Delay(3000);
+
+            int i = 0;
+
+            //Task.Run(() => ShowThreadInfo("Task"));
+            await Task.Run(() =>
+            {
+                //影像處理程序 把Do While 刪除 寫在這
+                //測試用
+                do
+                {
+                    i++;
+                    //Text = i.ToString();
+                    FormUpdata formcontrl = new FormUpdata(FormUpdataMethod);
+                    this.Invoke(formcontrl, i);
+
+                } while (i < 1000000000);
+
+            });
+
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DrawCanvas.Parent = CCDImage;
+            ROIImage.Parent = panel1;
+            //初始化 ROI位置
+            ROI_Point.X = 10;
+            ROI_Point.Y = 10;
+            DrawCanvas.Left = ROI_Point.X;
+            DrawCanvas.Top = ROI_Point.Y;
+
+            CCDImage.Width = 640;
+            CCDImage.Height = 360;
+            panel1.Width = 640;
+            panel1.Height = 360;
+            ROIImage.Width = 640;
+            ROIImage.Height = 360;
+
+            /*
+            DrawCanvas.Top = 0;
+            DrawCanvas.Left = 0;
+            DrawCanvas.Width = CCDImage.Width;
+            DrawCanvas.Height = CCDImage.Height;
+            */
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+
+            if (bCameraLive == false)
+            {
+                bCameraLive = true;
+                camera.Start();
+                
+                //var bmp = camera.GetBitmap();
+                btnStart.Text = "Stop";
+                // show image in PictureBox.
+                timer1.Start();
+
+            }
+            else
+            {
+                bCameraLive = false;
+                btnStart.Text = "Start";
+                timer1.Stop();
+                camera.Stop();
+            }
+
+        }
+        private static int inTimer = 0;
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            //Text = camera.Size.Width.ToString() + "," + camera.Size.Height.ToString();
+            Bitmap myBitmap = camera.GetBitmap();
+
+            int scaleX = camera.Size.Width / CCDImage.Width;
+            int scaleY = camera.Size.Height / CCDImage.Height;
+
+            int x = DrawCanvas.Left * scaleX;
+            int y = DrawCanvas.Top * scaleY;
+            int w = DrawCanvas.Width * scaleX;
+            int h = DrawCanvas.Height * scaleY;
+            RectangleF cloneRect = new RectangleF(x, y, w, h);
+            System.Drawing.Imaging.PixelFormat format = myBitmap.PixelFormat;
+
+            Bitmap cloneBitmap = myBitmap.Clone(cloneRect, format);
+
+            CCDImage.Image = myBitmap;
+            ROIImage.Image = cloneBitmap;
+
+            ROIImage.Left = DrawCanvas.Left;
+            ROIImage.Top = DrawCanvas.Top;
+            ROIImage.Height = DrawCanvas.Height;
+            ROIImage.Width = DrawCanvas.Width;
+
+            byte[] ROIBuffer = ImageToBuffer(ROIImage.Image, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            if (Interlocked.Exchange(ref inTimer, 1) == 1)
+                return;
+
+            if (ROIBuffer == null)
+            {
+                Interlocked.Exchange(ref inTimer, 0);
+                return;
+            }
+
+
+            Text = "Process...";
+
+
+            //Image Process
+            var  processTask = SpertroImageProcess(ROIBuffer);
+            Task processFinishTask = await Task.WhenAny(processTask);
+
+            Interlocked.Exchange(ref inTimer, 0);
+
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            int cameraIndex = 0;
+            // check format.
+            string[] devices = UsbCamera.FindDevices();
+            if (devices.Length == 0) return; // no camera.
+
+            UsbCamera.VideoFormat[] formats = UsbCamera.GetVideoFormat(cameraIndex);
+            //for (int i = 0; i < formats.Length; i++) Console.WriteLine("{0}:{1}", i, formats[i]);
+
+            // create usb camera and start.
+            camera = new UsbCamera(cameraIndex, formats[0]);
+        }
+
+        int iDirectionLock = -1;
+        private void DrawCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            PictureBox pDrawROI = (PictureBox)sender;
+            
+
+            if (e.Button.Equals(MouseButtons.Left))
+            {
+                /*
+                if (e.X < ROI_Edge)
+                {
+                    iDirectionLock = 1;
+                }
+                else */if (e.X > pDrawROI.Width - ROI_Edge)
+                {
+                    iDirectionLock = 2;
+                }
+                /*
+                else if (e.Y < ROI_Edge)
+                {
+                    iDirectionLock = 3;
+                }
+                */
+                else if (e.Y > pDrawROI.Height - ROI_Edge)
+                {
+                    iDirectionLock = 4;
+                }
+                else 
+                {
+                    //移動 Top Left
+
+                    iDirectionLock = 0;
+                }
+                ROI_Point = e.Location;
+            }
+        }
+
+
+        private void DrawCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            PictureBox pDrawROI = (PictureBox)sender;
+            //bool bEdge = false;
+            /*
+            if (e.X < ROI_Edge)
+            {
+                pDrawROI.Cursor = Cursors.PanWest;
+            }
+            else */if (e.X > pDrawROI.Width - ROI_Edge)
+            {
+                pDrawROI.Cursor = Cursors.PanEast;
+            }
+            /*
+            else if (e.Y < ROI_Edge)
+            {
+                pDrawROI.Cursor = Cursors.PanNorth;
+            }
+            */
+            else if (e.Y > pDrawROI.Height - ROI_Edge)
+            {
+                pDrawROI.Cursor = Cursors.PanSouth;
+            }
+            else
+            {
+                pDrawROI.Cursor = Cursors.Cross;
+            }
+            int X = pDrawROI.Left;
+            int Y = pDrawROI.Top;
+            int W = pDrawROI.Width;
+            int H = pDrawROI.Height; 
+
+            if (e.Button.Equals(MouseButtons.Left))
+            {
+                if (iDirectionLock == 1)
+                {
+
+                    X -= ROI_Point.X - e.Location.X;
+                    W += ROI_Point.X - e.Location.X;
+
+                }
+                else if (iDirectionLock == 2)
+                {
+                    W += e.Location.X - ROI_Point.X;
+                }
+
+                else if (iDirectionLock == 3)
+                {
+                    X -= ROI_Point.Y - e.Location.Y;
+                    H += ROI_Point.Y - e.Location.Y;
+
+                }
+
+                else if (iDirectionLock == 4)
+                {
+                    H += e.Location.Y - ROI_Point.Y;
+
+                }
+                
+                if (iDirectionLock == 0)
+                {
+                    X += e.Location.X - ROI_Point.X;
+                    Y += e.Location.Y - ROI_Point.Y;
+
+                }
+                else
+                {
+                    if (iDirectionLock == 1)
+                    {
+                        
+                        ROI_Point.Y = e.Location.Y;
+                    }
+                    else if (iDirectionLock == 2)
+                    {
+                        ROI_Point.X = e.Location.X;
+                        ROI_Point.Y = e.Location.Y;
+                    }
+                    else if (iDirectionLock == 3)
+                    {
+                        ROI_Point.X = e.Location.X;
+                       
+                    }
+                    else if (iDirectionLock == 4)
+                    {
+                        ROI_Point.X = e.Location.X;
+                        ROI_Point.Y = e.Location.Y;
+                    }     
+                }
+
+                if (X >= 0 && X+W <= CCDImage.Width)
+                    pDrawROI.Left = X;
+                if (Y >= 0 && Y+H <= CCDImage.Height)
+                    pDrawROI.Top = Y;
+                if (X + W >= 0 && X + W <= CCDImage.Width)
+                    pDrawROI.Width = W;
+                if (Y + H >= 0 && Y + H <= CCDImage.Height)
+                    pDrawROI.Height = H;
+            }
+        }
+
+        private void DrawCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            iDirectionLock = -1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+     
+}
+
+
